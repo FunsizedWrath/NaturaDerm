003143660f509c2048a2f963131991686698eda7
FunsizedWrath <151151740+FunsizedWrath@users.noreply.github.com>
2024-12-05 09:22:20 +0100

    created 2 different init file (one with schemas one without)

--- initDB/initDBwithchemas.sql
+++ initDB/initDBwithchemas.sql
@@ -1,4 +1,9 @@
-CREATE TABLE client(
+CREATE SCHEMA vente;
+GO
+CREATE SCHEMA manager;
+GO
+
+CREATE TABLE vente.client(
    idClient INT,
    nomClient VARCHAR(50) NOT NULL,
    prenomClient VARCHAR(50) NOT NULL,
@@ -15,7 +20,7 @@ CREATE TABLE client(
    CHECK (LEN(CAST(telClient AS VARCHAR(10))) = 10)
 );
 
-CREATE TABLE conseiller(
+CREATE TABLE vente.conseiller(
    idConseiller INT,
    nomConseiller VARCHAR(50) NOT NULL,
    prenomConseiller VARCHAR(50) NOT NULL,
@@ -30,7 +35,7 @@ CREATE TABLE conseiller(
    CHECK (LEN(CAST(telConseiller AS VARCHAR(10))) = 10)
 );
 
-CREATE TABLE fournisseur(
+CREATE TABLE manager.fournisseur(
    idFournisseur INT,
    nomFournisseur VARCHAR(50) NOT NULL,
    paysFournisseur VARCHAR(50) NOT NULL,
@@ -45,7 +50,7 @@ CREATE TABLE fournisseur(
    CHECK (LEN(CAST(telFournisseur AS VARCHAR(10))) = 10)
 );
 
-CREATE TABLE produit(
+CREATE TABLE vente.produit(
    referenceProduit VARCHAR(50),
    nomProduit VARCHAR(50) NOT NULL,
    prixHorsTaxeProduit Money NOT NULL,
@@ -56,10 +61,10 @@ CREATE TABLE produit(
    quantiteStockProduit INT,
    idFournisseur INT NOT NULL,
    PRIMARY KEY(referenceProduit),
-   FOREIGN KEY(idFournisseur) REFERENCES fournisseur(idFournisseur)
+   FOREIGN KEY(idFournisseur) REFERENCES manager.fournisseur(idFournisseur)
 );
 
-CREATE TABLE manager(
+CREATE TABLE manager.manager(
    idManager INT,
    nomManager VARCHAR(50) NOT NULL,
    prenomManager VARCHAR(50) NOT NULL,
@@ -67,7 +72,7 @@ CREATE TABLE manager(
    PRIMARY KEY(idManager)
 );
 
-CREATE TABLE commande(
+CREATE TABLE vente.commande(
    idCommande INT,
    remiseCommande Money,
    dateCommande DATE,
@@ -77,12 +82,12 @@ CREATE TABLE commande(
    idConseiller INT,
    idClient INT,
    PRIMARY KEY(idCommande),
-   FOREIGN KEY(idManager) REFERENCES manager(idManager),
-   FOREIGN KEY(idConseiller) REFERENCES conseiller(idConseiller),
-   FOREIGN KEY(idClient) REFERENCES client(idClient)
+   FOREIGN KEY(idManager) REFERENCES manager.manager(idManager),
+   FOREIGN KEY(idConseiller) REFERENCES vente.conseiller(idConseiller),
+   FOREIGN KEY(idClient) REFERENCES vente.client(idClient)
 );
 
-CREATE TABLE composant(
+CREATE TABLE vente.composant(
    idComposant INT,
    nomComposant VARCHAR(50) NOT NULL,
    estAllergene BIT NOT NULL,
@@ -90,41 +95,41 @@ CREATE TABLE composant(
    UNIQUE(nomComposant)
 );
 
-CREATE TABLE reunionClient(
+CREATE TABLE vente.reunionClient(
    idClient INT,
    idConseiller INT,
    idReunion INT NOT NULL,
    dateReunion DATE,
    PRIMARY KEY(idClient, idConseiller),
    UNIQUE(idReunion),
-   FOREIGN KEY(idClient) REFERENCES client(idClient),
-   FOREIGN KEY(idConseiller) REFERENCES conseiller(idConseiller)
+   FOREIGN KEY(idClient) REFERENCES vente.client(idClient),
+   FOREIGN KEY(idConseiller) REFERENCES vente.conseiller(idConseiller)
 );
 
-CREATE TABLE formationConseiller(
+CREATE TABLE vente.formationConseiller(
    idConseiller INT,
    idConseiller_1 INT,
    idFormation INT NOT NULL,
    dateFormation DATE,
    PRIMARY KEY(idConseiller, idConseiller_1),
    UNIQUE(idFormation),
-   FOREIGN KEY(idConseiller) REFERENCES conseiller(idConseiller),
-   FOREIGN KEY(idConseiller_1) REFERENCES conseiller(idConseiller)
+   FOREIGN KEY(idConseiller) REFERENCES vente.conseiller(idConseiller),
+   FOREIGN KEY(idConseiller_1) REFERENCES vente.conseiller(idConseiller)
 );
 
-CREATE TABLE estCommande(
+CREATE TABLE vente.estCommande(
    referenceProduit VARCHAR(50),
    idCommande INT,
    quantiteProduit INT NOT NULL,
    PRIMARY KEY(referenceProduit, idCommande),
-   FOREIGN KEY(referenceProduit) REFERENCES produit(referenceProduit),
-   FOREIGN KEY(idCommande) REFERENCES commande(idCommande)
+   FOREIGN KEY(referenceProduit) REFERENCES vente.produit(referenceProduit),
+   FOREIGN KEY(idCommande) REFERENCES vente.commande(idCommande)
 );
 
-CREATE TABLE compose(
+CREATE TABLE vente.compose(
    referenceProduit VARCHAR(50),
    idComposant INT,
    PRIMARY KEY(referenceProduit, idComposant),
-   FOREIGN KEY(referenceProduit) REFERENCES produit(referenceProduit),
-   FOREIGN KEY(idComposant) REFERENCES composant(idComposant)
+   FOREIGN KEY(referenceProduit) REFERENCES vente.produit(referenceProduit),
+   FOREIGN KEY(idComposant) REFERENCES vente.composant(idComposant)
 );
